9f7aeb4681eeea5e6cc0408b833d080ff1d953a9
Mark Chigrin <ILMPC@ya.ru>
2021-03-07 15:41:35 +0000

    Config project

---
+++ DoubleLinkedList.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+using AlgorithmsDataStructures;
+
+namespace DoubleLinkedList.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
