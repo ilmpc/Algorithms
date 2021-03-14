0b70fcecd757214afd73e996b7fc16fe73d8fb99
Mark Chigrin <ILMPC@ya.ru>
2021-03-14 16:09:48 +0000

    LIFO Stack

--- Stack/Stack.cs
+++ Stack/Stack.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Stack
+namespace AlgorithmsDataStructures
 {
-    public class Class1
+
+    public class Stack<T>
     {
+        private List<T> data;
+        public Stack()
+        {
+            data = new List<T>();
+        }
+
+        public int Size()
+        {
+            return data.Count;
+        }
+
+        public T Pop()
+        {
+            if (data.Count == 0) { return default(T); }
+            var last = data.Count - 1;
+            var item = data[last];
+            data.RemoveAt(last);
+            return item;
+        }
+
+        public void Push(T item)
+        {
+            data.Add(item);
+        }
+
+        public T Peek()
+        {
+            if (data.Count == 0) { return default(T); }
+            var last = data.Count - 1;
+            var item = data[last];
+            return item;
+        }
     }
+
 }
