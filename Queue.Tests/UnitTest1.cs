bf28367f774e7374bc7c340c4bcafeeeb18d178e
Mark Chigrin <ILMPC@ya.ru>
2021-03-15 09:29:09 +0000

    Add one more test

--- Queue.Tests/UnitTest1.cs
+++ Queue.Tests/UnitTest1.cs
@@ -11,8 +11,25 @@ namespace Queue.Tests
         public void Empty()
         {
             var q = new Queue<int>();
+            Assert.Equal(0, q.Size());
             q.Enqueue(5);
             Assert.Equal(5, q.Dequeue());
+            Assert.Equal(0, q.Size());
+            Assert.Equal(default(int), q.Dequeue());
+        }
+        [Fact]
+        public void EnqueueThenDequeueOrder()
+        {
+            var q = new Queue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                q.Enqueue(i);
+            }
+            Assert.Equal(10, q.Size());
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.Equal(i, q.Dequeue());
+            }
         }
     }
 }
