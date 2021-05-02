1579f9591932267105f87d507ac05ab0615b2534
Mark Chigrin <dev@chigrin.ml>
2021-05-02 16:22:58 +0000

    Remove hashtable from project

--- Main/Program.cs
+++ Main/Program.cs
@@ -7,9 +7,6 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            var table = new HashTable(17, 3);
-            Console.WriteLine(table.HashFun("0123456789"));
-            Console.WriteLine(table.HashFun("abcdefgh"));
         }
     }
 }
