6cf20076e2bc42463c7ae948851f7e6e7f1f5847
Mark Chigrin <dev@chigrin.ml>
2021-05-02 16:18:53 +0000

    Initial non working version

---
+++ PowerSet.Tests/UnitTest1.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Linq;
+using Xunit;
+
+using AlgorithmsDataStructures;
+
+namespace PowerSet.Tests
+{
+    public class UnitTest1
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Range(1, length).Select(_ => chars[random.Next(chars.Length)]).ToArray());
+        }
+
+        [Fact]
+        public void PutTwice()
+        {
+            var data = "Hi!";
+            var set = new PowerSet<string>();
+            set.Put(data);
+            Assert.Equal(1, set.Size());
+            set.Put(data);
+            Assert.Equal(1, set.Size());
+        }
+
+        [Fact]
+        public void RemoveSomething()
+        {
+            var data = "Hi!";
+            var set = new PowerSet<string>();
+            set.Put(data);
+            Assert.True(set.Get(data));
+            set.Remove(data);
+            Assert.Equal(0, set.Size());
+            Assert.False(set.Get(data));
+        }
+
+        [Fact]
+        public void Intersection()
+        {
+            var data = "Hi!";
+            var set1 = new PowerSet<string>();
+            var set2 = new PowerSet<string>();
+            set1.Put(data);
+
+        }
+        [Fact]
+        public void Performance()
+        {
+            var set1 = new PowerSet<string>();
+            var set2 = new PowerSet<string>();
+            for (int i = 0; i < 5000; i++)
+            {
+                var data = RandomString(10);
+                set1.Put(data);
+                set2.Put(data);
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                var data = RandomString(12);
+                set1.Put(data);
+                data = RandomString(12);
+                set2.Put(data);
+            }
+            Measure((n) => set1.Difference(set2));
+            Measure((n) => set1.Union(set2));
+            Measure((n) => set1.IsSubset(set2));
+            Measure((n) => set1.Intersection(set2));
+        }
+
+        private void Measure(Action<int> measurable)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            measurable(0);
+            watch.Stop();
+            Assert.True(watch.ElapsedMilliseconds < 1000);
+        }
+    }
+}
---
+++ PowerSet/PowerSet.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class PowerSet<T> where T : IEquatable<T>
+    {
+        private int size;
+        private int capacity;
+        private int step;
+        private T[] slots;
+
+        private PowerSet(PowerSet<T> other)
+        {
+            this.size = other.size;
+            this.capacity = other.capacity;
+            this.step = other.step;
+            this.slots = (T[])other.slots.Clone();
+
+        }
+        public PowerSet(int capacity = 20000, int step = 32)
+        {
+            this.size = 0;
+            this.capacity = capacity;
+            this.step = step;
+            this.slots = new T[capacity];
+            for (var i = 0; i < capacity; i++)
+            {
+                slots[i] = default(T);
+            }
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+        private int HashFun(string value)
+        {
+            if (value == null) { return 0; }
+            // Should be any prime number roughly equal to the number of different characters use
+            int p = 31;
+            int m = (int)(1e9 + 9);
+            long power_of_p = 1;
+            long hash = 0;
+
+            // Using Polynomial rolling 
+            foreach (char ch in value)
+            {
+                hash = (hash + Math.Abs(ch - 'a' + 1) * power_of_p) % m;
+                power_of_p = (power_of_p * p) % m;
+            }
+            return (int)(hash % capacity);
+        }
+
+        private int HashFun(T value)
+        {
+            var hash = value.GetHashCode();
+            return (int)(hash % capacity);
+        }
+
+        public void Put(T value)
+        {
+            if (capacity == 0 || size == capacity) { return; }
+
+            var hash = HashFun(value);
+            var potential = hash;
+            do
+            {
+                var exists = EqualityComparer<T>.Default.Equals(value, slots[potential]);
+                var empty = EqualityComparer<T>.Default.Equals(default(T), slots[potential]);
+                if (exists) { return; }
+                if (empty)
+                {
+                    slots[potential] = value;
+                    size += 1;
+                    return;
+                }
+                potential = (potential + step) % capacity;
+            } while (potential != hash);
+        }
+
+        public bool Get(T value)
+        {
+            if (capacity == 0) { return false; }
+
+            var hash = HashFun(value);
+            var potential = hash;
+            do
+            {
+                var exists = EqualityComparer<T>.Default.Equals(value, slots[potential]);
+                if (exists) { return true; }
+                potential = (potential + step) % capacity;
+            } while (potential != hash);
+            return false;
+        }
+
+        public bool Remove(T value)
+        {
+            if (capacity == 0) { return false; }
+
+            var hash = HashFun(value);
+            var potential = hash;
+            do
+            {
+                var exists = EqualityComparer<T>.Default.Equals(value, slots[potential]);
+                if (exists)
+                {
+                    slots[potential] = default(T);
+                    size -= 1;
+                    return true;
+                }
+                potential = (potential + step) % capacity;
+            } while (potential != hash);
+            return false;
+        }
+
+        public PowerSet<T> Intersection(PowerSet<T> other)
+        {
+            var intersection = new PowerSet<T>();
+            if (size == 0 || other.size == 0) { return intersection; }
+
+            foreach (var item in slots)
+            {
+                if (other.Get(item))
+                {
+                    intersection.Put(item);
+                }
+            }
+            return intersection;
+        }
+
+        public PowerSet<T> Union(PowerSet<T> other)
+        {
+            if (size == 0) { return new PowerSet<T>(other); }
+            if (other.size == 0) { return new PowerSet<T>(this); }
+
+            var union = new PowerSet<T>();
+
+            Action<T> put = (item) => union.Put(item);
+            Array.ForEach(slots, put);
+            Array.ForEach(other.slots, put);
+
+            return union;
+        }
+
+        public PowerSet<T> Difference(PowerSet<T> other)
+        {
+            if (size == 0) { return new PowerSet<T>(); }
+            if (other.size == 0) { return new PowerSet<T>(this); }
+
+            var difference = new PowerSet<T>();
+
+            foreach (var item in slots)
+            {
+                if (!other.Get(item))
+                {
+                    difference.Put(item);
+                }
+            }
+
+            return difference;
+        }
+
+        public bool IsSubset(PowerSet<T> other)
+        {
+            foreach (var item in other.slots)
+            {
+                if (!this.Get(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
